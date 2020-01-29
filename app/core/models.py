647f8ca048e43f36543e1b5ddf10de883e55031f
Devesh Patel <deveshp530@gmail.com>
2020-01-28 17:58:10 -0800

    updated admin to support custom user model

--- app/core/models.py
+++ app/core/models.py
@@ -10,7 +10,7 @@ class UserManager(BaseUserManager):
             raise ValueError('Must have valid email')
         user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
-        user.save(using=self.db)
+        user.save(using=self._db)
 
         return user
     
@@ -19,7 +19,7 @@ class UserManager(BaseUserManager):
         user = self.create_user(email, password)
         user.is_staff=True
         user.is_superuser = True
-        user.save(using=self.db)
+        user.save(using=self._db)
 
         return user
 
